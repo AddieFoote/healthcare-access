c210436851236d45a17248d19f4c785b3180343f
AddieFoote <addiefoote8@gmail.com>
2022-11-03 18:43:54 -0500

    some frontend/navigation

--- medscan-app/App.js
+++ medscan-app/App.js
@@ -1,8 +1,72 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, Touchable, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Welcome to PillPharm</Text>
+      <TouchableOpacity
+      style={styles.scanButton}
+      onPress={() => navigation.navigate('Scanner')}
+      >
+        <Text>Scan Barcode</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+      style={styles.scanButton}
+      onPress={() => navigation.navigate('Search')}
+      >
+        <Text>Search Name</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  return (
+    <NavigationContainer>{
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Scanner" component={Scanner} />
+        <Stack.Screen name="Search" component={Search} />
+
+      </Stack.Navigator>
+    }</NavigationContainer>
+  );
+}
+
+
+function Search() {
+  const [inputText, onChangeText] = React.useState(null);
+  
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={inputText}
+        placeholder="Type medication here"
+        placeholderTextColor="#000" 
+      />
+      
+      <TouchableOpacity
+      style={styles.scanButton}
+      onPress={() => Alert.alert("searched")}
+      >
+        <Text>Search</Text>
+      </TouchableOpacity>    
+    </View>
+  );
+
+}
+
+
+function Scanner() {
+  
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
@@ -45,4 +109,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  scanButton: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+  input: {
+    backgroundColor: "#EEEEEE",
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
 });
