e6b63a7712069e23e505064da185360805b3a06d
AddieFoote <addiefoote8@gmail.com>
2022-11-10 19:06:57 -0600

    files

--- medscan-app/App.js
+++ medscan-app/App.js
@@ -3,26 +3,10 @@ import { Text, View, StyleSheet, Button, Touchable, TouchableOpacity, TextInput,
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from "Screens/HomeScreen"
+import Search from "Screens/SearchScreen"
+import Scanner from "Screens/ScannerScreen"
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Welcome to PillPharm</Text>
-      <TouchableOpacity
-      style={styles.scanButton}
-      onPress={() => navigation.navigate('Scanner')}
-      >
-        <Text>Scan Barcode</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-      style={styles.scanButton}
-      onPress={() => navigation.navigate('Search')}
-      >
-        <Text>Search Name</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
 
 const Stack = createNativeStackNavigator();
 
@@ -39,68 +23,6 @@ export default function App() {
   );
 }
 
-
-function Search() {
-  const [inputText, onChangeText] = React.useState(null);
-  
-  return (
-    <View>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={inputText}
-        placeholder="Type medication here"
-        placeholderTextColor="#000" 
-      />
-
-      <TouchableOpacity
-      style={styles.scanButton}
-      onPress={() => Alert.alert("searched")}
-      >
-        <Text>Search</Text>
-      </TouchableOpacity>    
-    </View>
-  );
-
-}
-
-
-function Scanner() {
-  
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-
-  useEffect(() => {
-    const getBarCodeScannerPermissions = async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    };
-
-    getBarCodeScannerPermissions();
-  }, []);
-
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
-
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-
-  return (
-    <View style={styles.container}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
