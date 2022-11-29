6a87bbd1b4ad224fd12b016e322be4deaa827bbf
AddieFoote <addiefoote8@gmail.com>
2022-11-28 20:53:09 -0600

    info page

--- medscan-app/Screens/InfoScreen.js
+++ medscan-app/Screens/InfoScreen.js
@@ -1,38 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { styles } from '../Styles';
 
 export default function InfoScreen({route, nativation}) {
     const { name } = route.params;
+    const [dosageVis, setDosageVis] = useState(false);
+    const [interactionsVis, setInteractionsVis] = useState(false);
+    const [howToTakeVis, setHowToTakeVis] = useState(false);
+    const [symptomsVis, setSymptomsVis] = useState(false);
+    const [callVis, setCallVis] = useState(false);
+    const [dosage, setDose] = useState("afsjlk;gdhndf dksf");
+    const [interactions, setInteractions] = useState("afsjlk;gdhndf dksf");
+    const [howToTake, setHowToTake] = useState("afsjlk;gdhndf dksf");
+    const [symptoms, setSymptoms] = useState("afsjlk;gdhndf dksf");
+    const [call, setCall] = useState("afsjlk;gdhndf dksf");
 
     return (
+
         <View style={styles.viewsBackground} >
             <TouchableOpacity style={styles.infoButtons}>
-                <Text>{name}</Text>
+                <Text style={styles.title}>{name}</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.infoButtons}>
-                <Text>Dosage Information</Text>
+            <TouchableOpacity style={styles.infoButtons} onPress={() => setDosageVis(!dosageVis)}>
+                <Text style={styles.titleTextStyle}>Dosage Information</Text>
+                { dosageVis ? <Text style={styles.infoTextStyle}>{dosage}</Text> : null }
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.infoButtons}>
-                <Text>Interactions</Text>
+            <TouchableOpacity style={styles.infoButtons} onPress={() => setInteractionsVis(!interactionsVis)}>
+                <Text style={styles.titleTextStyle}>Interactions</Text>
+                { interactionsVis ? <Text style={styles.infoTextStyle}>{interactions}</Text> : null }
+
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.infoButtons}>
-                <Text>How to Take</Text>
+            <TouchableOpacity style={styles.infoButtons} onPress={() => setHowToTakeVis(!howToTakeVis)}>
+                <Text style={styles.titleTextStyle}>How to Take</Text>
+                { howToTakeVis ? <Text style={styles.infoTextStyle}>{howToTake}</Text> : null }
+
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.infoButtons}>
-                <Text>Symptoms</Text>
+            <TouchableOpacity style={styles.infoButtons} onPress={() => setSymptomsVis(!symptomsVis)}>
+                <Text style={styles.titleTextStyle}>Symptoms</Text>
+                { symptomsVis ? <Text style={styles.infoTextStyle}>{symptoms}</Text> : null }
+
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.infoButtons}>
-                <Text>When to Call</Text>
+            <TouchableOpacity style={styles.infoButtons} onPress={() => setCallVis(!callVis)}>
+                <Text style={styles.titleTextStyle}>When to Call</Text>
+                { callVis ? <Text style={styles.infoTextStyle}>{call}</Text> : null }
+
             </TouchableOpacity>
 
         </View>
         
     );
+}
+
+function dropDown() {
 
 }
--- medscan-app/Styles.js
+++ medscan-app/Styles.js
@@ -14,19 +14,32 @@ export const styles = StyleSheet.create({
       margin: 10
     },
     input: {
-      backgroundColor: "#EEEEEE",
       height: 40,
       margin: 12,
       borderWidth: 1,
       padding: 10,
     },
     infoButtons: {
-      alignItems: "center",
-      backgroundColor: "#DDDDDD",
-
+      alignItems: "left",
+      borderColor: "#D3D3D3",
+      borderBottomWidth: 1,
+      marginLeft: 20,
+      marginRight: 20,
+      padding: 10
     }, 
     viewsBackground: {
       backgroundColor: '#FFF9E8'
+    },
+    infoTextStyle: {
+      fontSize: 12,
+      lineHeight: 14
+    }, 
+    titleTextStyle:{
+      fontSize: 18, 
+      padding: 14
+    }, 
+    title:{
+      fontSize: 24
     }
   });
   
