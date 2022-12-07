2ead3b45f5dac760a0e1f643fa882bce5dfae399
AddieFoote <addiefoote8@gmail.com>
2022-12-06 22:52:19 -0600

    commit info functionality

--- medscan-app/Screens/InfoScreen.js
+++ medscan-app/Screens/InfoScreen.js
@@ -2,6 +2,18 @@ import React, { useState } from 'react';
 import { Text, View, Image, InlineImage, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { styles } from '../Styles';
 
+const barcodeMap = new Map();
+barcodeMap.set("0300450488268", "Tylenol");
+barcodeMap.set("30040352", "Tylenol");
+
+const infoMap = new Map();
+infoMap.set("Tylenol", ["For Adults: 1 pill every 4 to 6 hours. Do not take more than 12 pills in a day (24 hours). If pain or fever does not respond to 1 pill, take 2 pills at the next dose.", 
+"The side effects of Tylenol (acetaminophen) include nausea, stomach pain, headache, loss of appetite, itching, rash, dark urine, clay-colored stools and swelling of the face, throat, tongue or limbs."
+ ,"Ask your doctor before using acetaminophen together with ethanol. This can cause serious side effects that affect your liver."
+, "Contraindications to using acetaminophen include hypersensitivity to acetaminophen, severe hepatic impairment, or severe active hepatic disease."
+, "Acetaminophen is an analgesic drug used alone or in combination with opioids for pain management, and as an antipyretic agent."
+,"Acetaminophen is an analgesic drug" ])
+
 export default function InfoScreen({route, nativation}) {
     const { name } = route.params;
     const [dosageVis, setDosageVis] = useState(false);
@@ -10,12 +22,13 @@ export default function InfoScreen({route, nativation}) {
     const [symptomsVis, setSymptomsVis] = useState(false);
     const [callVis, setCallVis] = useState(false);
     const [callPics, setCallPics] = useState(false);
-    const [dosage, setDose] = useState("For Adults: 1 pill every 4 to 6 hours. Do not take more than 12 pills in a day (24 hours). If pain or fever does not respond to 1 pill, take 2 pills at the next dose.");
-    const [interactions, setInteractions] = useState("The side effects of Tylenol (acetaminophen) include nausea, stomach pain, headache, loss of appetite, itching, rash, dark urine, clay-colored stools and swelling of the face, throat, tongue or limbs.");
-    const [howToTake, setHowToTake] = useState("Ask your doctor before using acetaminophen together with ethanol. This can cause serious side effects that affect your liver.");
-    const [symptoms, setSymptoms] = useState("Contraindications to using acetaminophen include hypersensitivity to acetaminophen, severe hepatic impairment, or severe active hepatic disease.");
-    const [call, setCall] = useState("Acetaminophen is an analgesic drug used alone or in combination with opioids for pain management, and as an antipyretic agent.");
-    const [pics, setPics] = useState("Acetaminophen is an analgesic drug");
+
+    const [dosage, setDose] = useState(infoMap.get(name)[0]);
+    const [interactions, setInteractions] = useState(infoMap.get(name)[1]);
+    const [howToTake, setHowToTake] = useState(infoMap.get(name)[2]);
+    const [symptoms, setSymptoms] = useState(infoMap.get(name)[3]);
+    const [call, setCall] = useState(infoMap.get(name)[4]);
+    const [pics, setPics] = useState(infoMap.get(name)[5]);
 
     return (
 
