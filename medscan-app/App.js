b1485222d84c7b4b03c07b86f40146052350d9ba
AddieFoote <addiefoote8@gmail.com>
2022-12-06 20:53:06 -0600

    Update App.js

--- medscan-app/App.js
+++ medscan-app/App.js
@@ -14,9 +14,10 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>{
+      
       <Stack.Navigator>
+        
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Scanner" component={Scanner} />
         <Stack.Screen name="Search" component={Search} />
         <Stack.Screen name="Info" component={InfoScreen} />
@@ -26,3 +27,4 @@ export default function App() {
     }</NavigationContainer>
   );
 }
+//        <Stack.Screen name="Home" component={HomeScreen}/>
